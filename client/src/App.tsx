25091e36f5a0cd3e75a11e4a04f7131ee54f718e
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-09-07 22:12:26 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,6 +27,7 @@ function App() {
         <Route path="/restaurant/result" element={<ReResult />} />
         <Route path="/recycle" element={<Recycle />} />
         <Route path="/recycle/result" element={<Rcresult />} />
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/help" element={<Help />} />
         <Route path="/allergy" element={<Allergy />} />
